cb208c07bd0253c2372d73b4c6198bd9b9fd6bd4
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2023-12-05 22:37:44 +0800

    add cut sentence

--- src/txt_spider.py
+++ src/txt_spider.py
@@ -20,20 +20,22 @@ TC_FONT_PATH = 'NotoSansTC-Regular.otf' # 繁體中文字型檔名
 
 # jieba斷詞
 # 精確模式
-for sentence in data:
-    seg_list = jieba.cut(sentence)
-    print('/'.join(seg_list))
+# for sentence in data:
+seg_list = jieba.lcut(data)
+print('/'.join(seg_list))
 
 print('---------------')
 
-# 全模式
-for sentence in data:
-    seg_list = jieba.cut(sentence, cut_all=True)
-    print('/'.join(seg_list))
+# # 全模式
+# for sentence in data:
+#     seg_list = jieba.cut(sentence, cut_all=True)
+#     print('/'.join(seg_list))
 
-print('---------------')
+# print('---------------')
 
-# 搜索引擎模式
-for sentence in data:
-    seg_list = jieba.cut_for_search(sentence)
-    print('/'.join(seg_list))
+# # 搜索引擎模式
+# for sentence in data:
+#     seg_list = jieba.cut_for_search(sentence)
+#     print('/'.join(seg_list))
+
+
