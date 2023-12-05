2392e34c2e43734fc24d8e6ec9caa62c56718030
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2023-12-05 21:49:55 +0800

    rename data

--- src/txt_spider.py
+++ src/txt_spider.py
@@ -3,17 +3,18 @@ import numpy                    # 分析模組
 import requests                 # 網路模組
 from collections import Counter # 次數統計模組
 
-# from google.colab import files  # colab檔案處理模組
 from PIL import Image           # 圖片處理模組
 import jieba                    # 分詞模組
 import matplotlib.pyplot as plt # 視覺化模組
 import wordcloud                # 文字雲模組
 
 with open('spider_zonggui_previous.txt', 'r', encoding='utf-8') as f:
-    data = f.read()
-    print(data)
+    zonggui_previous = f.read()
+    print(zonggui_previous)
     print('------------------')
 
 # 定義繁體中文檔名
 WORDS_PATH = 'dict.txt.big.txt' # 繁體中文詞庫檔名
 TC_FONT_PATH = 'NotoSansTC-Regular.otf' # 繁體中文字型檔名
+
+
